394e4eb6836697cd39fd61448d9fe3e11bbae6ed
Joe Ng'ethe <joeynimu@gmail.com>
2020-12-21 17:15:57 +0300

    full code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useGetLaunchesQuery } from './generated/graphql';
+import { useGetLaunchesQuery } from "./generated/graphql";
 
 function App() {
-
-
-  const {data, loading, error } = useGetLaunchesQuery()
-
+  const { data, loading, error } = useGetLaunchesQuery();
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  return <ul>{data?.launchesPast?.map(launch => launch?.ships?.map(ship => <li>{ship?.name}</li>))}</ul>
-
+  return (
+    <ul>
+      {data?.launchesPast?.map((launch) =>
+        launch?.ships?.map((ship) => <li key={ship?.name}>{ship?.name}</li>)
+      )}
+    </ul>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ApolloProvider } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/client";
 import client from "./apollo-client";
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
-      </ApolloProvider>
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
